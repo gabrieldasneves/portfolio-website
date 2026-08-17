27c9a240e65ff3d9576bef78b15c74e890c1dbb4
gabrieldasneves <galvesdasneves@gmail.com>
2026-08-17 14:54:55 +0900

    Add Next.js configuration file and update package-lock.json with new dependencies for Cloudflare support

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
-
-const { initOpenNextCloudflareForDev } = require('@opennextjs/cloudflare')
-initOpenNextCloudflareForDev()
+export default nextConfig
